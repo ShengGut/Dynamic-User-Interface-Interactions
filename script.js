8a9fa574a7647e48c59bbf9563a2404aff35c6fe
ShengGut <khy.l.jason@gmail.com>
2024-02-26 13:19:03 -0800

    Added dropdown menu tabs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+// Reusable function to toggle visibility of dropdown menus
+function toggleDropdown() {
+  var dropdowns = document.querySelectorAll('.dropdown')
+
+  dropdowns.forEach(function (dropdown) {
+    dropdown.addEventListener('click', function (event) {
+      var menu = this.querySelector('.dropdown-menu')
+      menu.classList.toggle('visible')
+    })
+  })
+}
+
+// Initialize dropdown toggles
+toggleDropdown()
