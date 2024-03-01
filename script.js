01744084c88dd2e577dbd60450db28d315338816
ShengGut <khy.l.jason@gmail.com>
2024-03-01 14:22:56 -0800

    Added mobile functionality for dropdown

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-// hovers over the menu and it will drop down the list of tabs
 function toggleDropdown() {
   let dropdowns = document.querySelectorAll('.dropdown')
 
   dropdowns.forEach(function (dropdown) {
     let menu = dropdown.querySelector('.dropdown-menu')
 
+    // For desktop devices
     dropdown.addEventListener('mouseover', function (event) {
       menu.style.display = 'block'
     })
+
     dropdown.addEventListener('mouseout', function (event) {
       menu.style.display = 'none'
+    })
+
+    // For mobile devices
+    dropdown.addEventListener('click', function (event) {
+      if (window.innerWidth <= 768) {
+        event.preventDefault()
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block'
+      }
+    })
+    // Improves fluidty of tap instead of having to click twice
+    dropdown.addEventListener('touchstart', function (event) {
+      if (window.innerWidth <= 768) {
+        event.preventDefault()
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block'
+      }
     })
   })
 }
