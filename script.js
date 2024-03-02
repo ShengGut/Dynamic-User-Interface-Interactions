626205eb4f8f665c9df069c6413d83fb298e7fea
ShengGut <khy.l.jason@gmail.com>
2024-03-01 17:38:04 -0800

    Finished writing image carousel

--- script.js
+++ script.js
@@ -49,3 +49,82 @@ function invertSVGColor() {
 }
 
 invertSVGColor()
+
+const track = document.querySelector('.carousel-track')
+const slides = Array.from(track.children)
+const nextButton = document.querySelector('.carousel-button--right')
+const prevButton = document.querySelector('.carousel-button--left')
+const dotsNav = document.querySelector('.carousel-nav')
+const dots = Array.from(dotsNav.children)
+console.log(slides)
+console.log(dots)
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px'
+}
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('current-slide')
+  targetDot.classList.add('current-slide')
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden')
+    nextButton.classList.remove('is-hidden')
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove('is-hidden')
+    nextButton.classList.add('is-hidden')
+  } else {
+    prevButton.classList.remove('is-hidden')
+    nextButton.classList.remove('is-hidden')
+  }
+}
+
+slides.forEach(setSlidePosition)
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')'
+  currentSlide.classList.remove('current-slide')
+  targetSlide.classList.add('current-slide')
+}
+
+// When clicking previous button, it moves left
+prevButton.addEventListener('click', (e) => {
+  const currentSlide = track.querySelector('.current-slide')
+  const prevSlide = currentSlide.previousElementSibling
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const prevDot = currentDot.previousElementSibling
+  const prevIndex = slides.findIndex((slide) => slide === prevSlide)
+  moveToSlide(track, currentSlide, prevSlide)
+  updateDots(currentDot, prevDot)
+  hideShowArrows(slides, prevButton, nextButton, prevIndex)
+})
+// When clicking next button, it moves right
+nextButton.addEventListener('click', (e) => {
+  const currentSlide = track.querySelector('.current-slide')
+  const nextSlide = currentSlide.nextElementSibling
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const nextDot = currentDot.nextElementSibling
+  const nextIndex = slides.findIndex((slide) => slide === nextSlide)
+  moveToSlide(track, currentSlide, nextSlide)
+  updateDots(currentDot, nextDot)
+  hideShowArrows(slides, prevButton, nextButton, nextIndex)
+})
+// When clicking on navdot, it moves to that slide
+dotsNav.addEventListener('click', (e) => {
+  const targetDot = e.target.closest('button')
+
+  if (!targetDot) return
+
+  const currentSlide = track.querySelector('.current-slide')
+  const currentDot = dotsNav.querySelector('.current-slide')
+  const targetIndex = dots.findIndex((dot) => dot === targetDot)
+  const targetSlide = slides[targetIndex]
+  moveToSlide(track, currentSlide, targetSlide)
+  updateDots(currentDot, targetDot)
+  console.log(targetIndex)
+  hideShowArrows(slides, prevButton, nextButton, targetIndex)
+})
