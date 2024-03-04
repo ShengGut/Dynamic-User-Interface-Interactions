42adff1f3304f038a557b03f1f06315519c6f852
ShengGut <khy.l.jason@gmail.com>
2024-03-04 11:58:59 -0800

    Refactored Javascript code

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 function toggleDropdown() {
-  let dropdowns = document.querySelectorAll('.dropdown')
-
-  dropdowns.forEach(function (dropdown) {
-    let menu = dropdown.querySelector('.dropdown-menu')
+  document.querySelectorAll('.dropdown').forEach((dropdown) => {
+    const menu = dropdown.querySelector('.dropdown-menu')
+    const toggleMenu = () =>
+      (menu.style.display = menu.style.display === 'block' ? 'none' : 'block')
 
     // For desktop devices
-    dropdown.addEventListener('mouseover', function (event) {
-      menu.style.display = 'block'
-    })
-
-    dropdown.addEventListener('mouseout', function (event) {
-      menu.style.display = 'none'
-    })
+    dropdown.addEventListener('mouseover', () => (menu.style.display = 'block'))
+    dropdown.addEventListener('mouseover', () => (menu.style.display = 'none'))
 
     // For mobile devices
-    dropdown.addEventListener('click', function (event) {
-      if (window.innerWidth <= 768) {
-        event.preventDefault()
-        menu.style.display = menu.style.display === 'block' ? 'none' : 'block'
-      }
-    })
+    dropdown.addEventListener(
+      'click',
+      (e) => window.innerWidth <= 768 && e.preventDefault() && toggleMenu()
+    )
     // Improves fluidty of tap instead of having to click twice
-    dropdown.addEventListener('touchstart', function (event) {
-      if (window.innerWidth <= 768) {
-        event.preventDefault()
-        menu.style.display = menu.style.display === 'block' ? 'none' : 'block'
-      }
-    })
+    dropdown.addEventListener(
+      'touchstart',
+      (e) => window.innerWidth <= 768 && e.preventDefault() && toggleMenu()
+    )
   })
 }
 
@@ -35,116 +26,98 @@ toggleDropdown()
 // checks if html theme is light or dark and inverts the SVG icons colors
 function invertSVGColor() {
   const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-  console.log('Is dark theme?', isDarkTheme)
-  if (isDarkTheme) {
-    const carouselButtons = document.querySelectorAll('.carousel-button')
-
-    carouselButtons.forEach((button) => {
-      const img = button.querySelector('img')
-      if (img) {
-        img.classList.add('inverted-svg')
-      }
-    })
-  }
+  document
+    .querySelectorAll('.carousel-button img')
+    .forEach((img) => isDarkTheme && img.classList.add('inverted-svg'))
 }
 
 invertSVGColor()
 
-const track = document.querySelector('.carousel-track')
-const slides = Array.from(track.children)
-const nextButton = document.querySelector('.carousel-button--right')
-const prevButton = document.querySelector('.carousel-button--left')
-const dotsNav = document.querySelector('.carousel-nav')
-const dots = Array.from(dotsNav.children)
-console.log(slides)
-console.log(dots)
+const startImageCarousel = () => {
+  const track = document.querySelector('.carousel-track')
+  const slides = Array.from(track.children)
+  const nextButton = document.querySelector('.carousel-button--right')
+  const prevButton = document.querySelector('.carousel-button--left')
+  const dotsNav = document.querySelector('.carousel-nav')
+  const dots = Array.from(dotsNav.children)
+  const slideWidth = slides[0].getBoundingClientRect().width
 
-const slideWidth = slides[0].getBoundingClientRect().width
-
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + 'px'
-}
-
-const updateDots = (currentDot, targetDot) => {
-  currentDot.classList.remove('current-slide')
-  targetDot.classList.add('current-slide')
-}
-
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-  if (targetIndex === 0) {
-    prevButton.classList.add('is-hidden')
-    nextButton.classList.remove('is-hidden')
-  } else if (targetIndex === slides.length - 1) {
-    prevButton.classList.remove('is-hidden')
-    nextButton.classList.add('is-hidden')
-  } else {
-    prevButton.classList.remove('is-hidden')
-    nextButton.classList.remove('is-hidden')
+  const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px'
   }
-}
+  slides.forEach(setSlidePosition)
 
-slides.forEach(setSlidePosition)
+  const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+  }
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = 'translateX(-' + targetSlide.style.left + ')'
-  currentSlide.classList.remove('current-slide')
-  targetSlide.classList.add('current-slide')
-}
+  const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
+  }
 
-// When clicking previous button, it moves left
-prevButton.addEventListener('click', (e) => {
-  const currentSlide = track.querySelector('.current-slide')
-  const prevSlide = currentSlide.previousElementSibling
-  const currentDot = dotsNav.querySelector('.current-slide')
-  const prevDot = currentDot.previousElementSibling
-  const prevIndex = slides.findIndex((slide) => slide === prevSlide)
-  moveToSlide(track, currentSlide, prevSlide)
-  updateDots(currentDot, prevDot)
-  hideShowArrows(slides, prevButton, nextButton, prevIndex)
-})
-// When clicking next button, it moves right
-nextButton.addEventListener('click', (e) => {
-  const currentSlide = track.querySelector('.current-slide')
-  const nextSlide = currentSlide.nextElementSibling
-  const currentDot = dotsNav.querySelector('.current-slide')
-  const nextDot = currentDot.nextElementSibling
-  const nextIndex = slides.findIndex((slide) => slide === nextSlide)
-  moveToSlide(track, currentSlide, nextSlide)
-  updateDots(currentDot, nextDot)
-  hideShowArrows(slides, prevButton, nextButton, nextIndex)
-})
-// When clicking on navdot, it moves to that slide
-dotsNav.addEventListener('click', (e) => {
-  const targetDot = e.target.closest('button')
-
-  if (!targetDot) return
-
-  const currentSlide = track.querySelector('.current-slide')
-  const currentDot = dotsNav.querySelector('.current-slide')
-  const targetIndex = dots.findIndex((dot) => dot === targetDot)
-  const targetSlide = slides[targetIndex]
-  moveToSlide(track, currentSlide, targetSlide)
-  updateDots(currentDot, targetDot)
-  console.log(targetIndex)
-  hideShowArrows(slides, prevButton, nextButton, targetIndex)
-})
-
-// Will advance slide every 5 second and reset once it reaches the end
-function autoAdvanceSlides() {
-  let currentIndex = 0
-  const interval = setInterval(() => {
-    const nextIndex = (currentIndex + 1) % slides.length // if currentIndex + 1 is equal to or greater, then wrap to the beginning of array
-    moveToSlide(track, slides[currentIndex], slides[nextIndex])
-    updateDots(dotsNav.querySelector('.current-slide'), dots[nextIndex])
-    hideShowArrows(slides, prevButton, nextButton, nextIndex)
-    currentIndex = nextIndex
-  }, 5000)
-  // Stop auto advancing when user interacts with slides, or restart auto advancing when user mouses out of it
-  track.addEventListener('mouseover', () => clearInterval(interval))
-  track.addEventListener('mouseout', () => {
-    clearInterval(interval)
-    autoAdvanceSlides()
+  const hideShowArrows = (targetIndex) => {
+    prevButton.classList.toggle('is-hidden', targetIndex === 0)
+    nextButton.classList.toggle('is-hidden', targetIndex === slides.length - 1)
+  }
+  // When clicking previous button, it moves left
+  prevButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.current-slide')
+    const prevSlide = currentSlide.previousElementSibling
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const prevDot = currentDot.previousElementSibling
+    const prevIndex = slides.findIndex((slide) => slide === prevSlide)
+    moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot)
+    hideShowArrows(prevIndex)
   })
+  // When clicking next button, it moves right
+  nextButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.current-slide')
+    const nextSlide = currentSlide.nextElementSibling
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const nextDot = currentDot.nextElementSibling
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide)
+    moveToSlide(track, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot)
+    hideShowArrows(nextIndex)
+  })
+  // When clicking on navdot, it moves to that slide
+  dotsNav.addEventListener('click', (e) => {
+    const targetDot = e.target.closest('button')
+    if (!targetDot) return
+
+    const currentSlide = track.querySelector('.current-slide')
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const targetIndex = dots.findIndex((dot) => dot === targetDot)
+    const targetSlide = slides[targetIndex]
+    moveToSlide(track, currentSlide, targetSlide)
+    updateDots(currentDot, targetDot)
+    console.log(targetIndex)
+    hideShowArrows(targetIndex)
+  })
+
+  // Will advance slide every 5 second and reset once it reaches the end
+  function autoAdvanceSlides() {
+    let currentIndex = 0
+    const interval = setInterval(() => {
+      const nextIndex = (currentIndex + 1) % slides.length // if currentIndex + 1 is equal to or greater, then wrap to the beginning of array
+      moveToSlide(track, slides[currentIndex], slides[nextIndex])
+      updateDots(dots[currentIndex], dots[nextIndex])
+      hideShowArrows(nextIndex)
+      currentIndex = nextIndex
+    }, 5000)
+    // Stop auto advancing when user interacts with slides, or restart auto advancing when user mouses out of it
+    track.addEventListener('mouseover', () => clearInterval(interval))
+    track.addEventListener('mouseout', () => {
+      clearInterval(interval)
+      autoAdvanceSlides()
+    })
+  }
+
+  autoAdvanceSlides()
 }
 
-autoAdvanceSlides()
+startImageCarousel()
